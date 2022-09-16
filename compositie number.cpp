54b617a5bbd3906c8e569c79cf27859980bb3863
tharaksreeram10 <112495765+tharaksreeram10@users.noreply.github.com>
2022-09-16 11:58:55 +0530

    Add files via upload

---
+++ compositie number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	
+   int fvalue, lvalue, i;
+   printf("Enter the number a and b\n");
+   scanf("%d%d",&fvalue,&lvalue);
+   for(i=fvalue;i<=lvalue;i++)
+   
+   {
+     printf("%d ",i);
+   }
+   return 0;
+}
